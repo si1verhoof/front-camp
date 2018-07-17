b07b5b49db844d9b2e90a198881ab3631dc3ba8d
si1verhoof <ftadsh@gmail.com>
2018-07-17 20:44:49 +0500

    Настройка автоматизации: галп+плагины

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var pug = require('gulp-pug');
+let cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('clean', function (cb) {
+  return gulp.src('build/', { allowEmpty: true, read: false })
+      .pipe(clean())
 });
+ 
+gulp.task('sass', function () {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('html', function(){
+  return gulp.src('pug/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build/'))
+});
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('html', 'minify-css', 'sass')));
